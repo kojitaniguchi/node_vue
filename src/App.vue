1cbf113338a91e25e74c5277387f5946f41e5b2c
kojitaniguchi <tanigu0511@gmail.com>
2017-11-16 21:48:07 +0900

    ADD store

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <vue-header></vue-header>
+    <time-line></time-line>
   </div>
 </template>
 
 <script>
+import VueHeader from './components/Header'
+import TimeLine from './components/TimeLine'
+
 export default {
-  name: 'app'
+  components: {
+    VueHeader,
+    TimeLine
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// // The Vue build version to load with the `import` command
+// // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+// import router from './router'
+// import store from './vuex/store'
+//
+// Vue.config.productionTip = false
+//
+// /* eslint-disable no-new */
+// new Vue({
+//   store,
+//   el: 'body',
+//   router,
+//   template: '<App/>',
+//   components: { App }
+// })
 
-Vue.config.productionTip = false
-
+import store from './vuex/store'
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
+  store,
+  el: 'body',
   components: { App }
 })
